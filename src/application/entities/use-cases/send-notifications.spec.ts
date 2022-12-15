5b0d41eae7afa20d7d51d1268e0ef263ee68ebb8
Junior <jos.pd2019@gmail.com>
2022-12-14 23:00:04 -0300

    in-memory-database pattern implemented on notifications repository

--- src/application/entities/use-cases/send-notifications.spec.ts
+++ src/application/entities/use-cases/send-notifications.spec.ts
@@ -1,22 +1,19 @@
+import { InMemoryNotificationsRepository } from "../../../../test/repositories/in-memory-notifications-repository";
 import { Notifications } from "../notifications";
 import { SendNotification } from "./send-notifications";
 
-const notificationsList: Notifications[] = [];
 
-const notificationsRepository = {
-    async create(notification: Notifications) {
-        notificationsList.push(notification);
-    }
-}
 describe('Send Notification', () => {
     it('Should send a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        
         const sendNotification = new SendNotification(notificationsRepository)
-        await sendNotification.execute({
+        const {notification} = await sendNotification.execute({
             recipientId: 'asdjfhakdjfhakdsjfh',
             category: 'Social',
             content: 'Hello, welcome user!'
         })
 
-        expect(notificationsList).toHaveLength(1);
+        expect(notificationsRepository.notificationsList[0]).toEqual(notification);
     },);
 },); 
